2ebeda7581f879e273d1e627dfd7341c94dd8726
emiianorojas <132502961+emiianorojas@users.noreply.github.com>
2023-05-03 14:53:49 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("hello")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+basic.forever(function () {
+	
+})
