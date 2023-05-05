58499d17ce16ffcd7158557d94eb521770e30502
emiianorojas <132502961+emiianorojas@users.noreply.github.com>
2023-05-05 12:42:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("hello")
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+    basic.showString("Hello!")
 })
 basic.forever(function () {
 	
